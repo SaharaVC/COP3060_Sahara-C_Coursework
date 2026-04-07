20b3276791b0b980e536642d912e97c87e50ede3
SaharaVC <ms.scalloway2006@gmail.com>
2026-04-06 20:01:47 -0400

    added variables, types, and operators

--- add.js
+++ add.js
@@ -1,2 +1,21 @@
 console.log("Welcome to Sahara Calloway's Coursework Page!");
 console.log("Repo: https://github.com/SaharaVC/COP3060_Sahara-C_Coursework");
+
+const ownerName = "Sahara Calloway";
+const interestCount = 5;
+const newsletterOptedIn = false;
+const topInterests = ["Music", "Video Games", "Anime", "Collecting DVDs", "Collecting Fugglers"];
+const ownerInfo = {
+  major: "Computer Science",
+  year: 2,
+  focus: "Cybersecurity"
+};
+let fetchedPosts = null;
+ 
+const totalChars = topInterests.reduce((sum, s) => sum + s.length, 0);
+ 
+const newsletterOff = newsletterOptedIn === false;
+ 
+if (ownerName.length > 0 && ownerInfo.major !== undefined) {
+  console.log(`Owner: ${ownerName} | Major: ${ownerInfo.major} | Total interest chars: ${totalChars}`);
+}
